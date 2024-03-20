22ac6455c8b1b28b97ef75f8dad720392a2c4b6f
martin-centurion <martinalejandrocenturion@gmail.com>
2024-03-20 17:56:09 -0300

    install components antd / bootstrap cdn

--- turnos/src/App.js
+++ turnos/src/App.js
@@ -1,27 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './Components/Login/login';
-import Register from './Components/Register/register';
-import './index.css';
-
 function App() {
   return (
-  <BrowserRouter>
     <div className="App">
-      <main>
-        
-        <Routes>
-          <Route path='/login' element={ <Login/> }/>
-          <Route path='/register' element={ <Register/> }/>
-        </Routes>
-      </main>
-
-
-
-
- 
+      <h1>DEMO</h1>
     </div>
-  </BrowserRouter>
   );
 }
 
