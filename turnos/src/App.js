e65a8280100222da09808e0a2e9c4fcbf8f4010a
martin-centurion <martinalejandrocenturion@gmail.com>
2024-03-21 02:23:54 -0300

    create home screen

--- turnos/src/App.js
+++ turnos/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/login';
 import Register from './Components/Register/register';
+import Home from './Components/Home/home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route path='/' element={ <Login /> } />
               <Route path='/register' element={ <Register /> } />
+              <Route path='/home' element={ <Home /> } />
           </Routes>
       </BrowserRouter>
     </div>
