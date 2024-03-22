3f04e12a0b601b5c025a3745ebb85f2056eaca43
Martu Riesco <marturiesco.costa@hotmail.com>
2024-03-22 17:28:18 -0300

    login and register conected to db

--- turnos/src/Components/Home/home.jsx
+++ turnos/src/Components/Home/home.jsx
@@ -5,7 +5,7 @@ function Home() {
 
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home Page</h1>
         </div>
     );
 };
--- turnos/src/Components/Register/register.jsx
+++ turnos/src/Components/Register/register.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles.css';
 import Logo from "../Logo/logo";
 import axios from 'axios';
 import toast from 'react-hot-toast'
 
 function Register() {
-
+const navigate = useNavigate()
     const onFinish = async(values) => {
        try {
         const response = await axios.post('/auth/register', values)
+        console.log(response);
         if (response.data.success) {
             toast.success(response.data.message)
+            toast('Redirecting to login page')
+            navigate('/')
         }else{
             toast.error(response.data.message)
         }
