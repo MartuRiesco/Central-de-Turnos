132299f72f65f1ccdcc8cb0f5a2394d53e6fdea3
Martu Riesco <marturiesco.costa@hotmail.com>
2024-04-29 18:54:02 -0300

    trying deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 app.use(express.json());
@@ -7,7 +8,12 @@ const userRoute = require('./routes/userRoute')
 const adminRoute = require('./routes/adminRoute')
 const employeeRoute = require('./routes/employeeRoute')
 
+app.use(express.static(path.join(__dirname, '../turnos/build')));
 
+// Ruta principal para servir el frontend
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../turnos/build/index.html'));
+});
 app.use('/api/user', userRoute);
 app.use('/api/admin', adminRoute);
 app.use('/api/employee', employeeRoute);
