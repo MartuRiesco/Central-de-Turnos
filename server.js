b7652782b31d729c51ea31c8ac1400ad391e4787
martin-centurion <martinalejandrocenturion@gmail.com>
2024-05-01 12:27:41 -0300

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Origin', ['https://central-de-turnos-7hvn.vercel.app']);
     res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     //res.append('Access-Control-Allow-Headers', 'Content-Type');
     next()
