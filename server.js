5f92b8a0756bda3f97941c5366463aae44172727
martin-centurion <martinalejandrocenturion@gmail.com>
2024-05-01 12:02:30 -0300

    headers backend

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ const corsOptions = {
   
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    //res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
+
 
 app.use('/api/user', userRoute);
 app.use('/api/admin', adminRoute);
