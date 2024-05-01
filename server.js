cc21faa2a303822f2da2811d8a565d6ab60d5254
martin-centurion <martinalejandrocenturion@gmail.com>
2024-05-01 11:27:07 -0300

    add cors

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 app.use(express.json());
+const cors = require('cors')
 const userRoute = require('./routes/userRoute')
 const adminRoute = require('./routes/adminRoute')
 const employeeRoute = require('./routes/employeeRoute')
 
-
+app.use(cors({
+    origin: ['https://central-de-turnos-7hvn.vercel.app']
+}))
 app.use('/api/user', userRoute);
 app.use('/api/admin', adminRoute);
 app.use('/api/employee', employeeRoute);
