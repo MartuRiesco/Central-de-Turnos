62282b3be350efca009ca14130cb98eb6ec2ef67
martin-centurion <martinalejandrocenturion@gmail.com>
2024-05-27 03:46:08 -0300

    add logo

--- turnos/src/components/Logo.js
+++ turnos/src/components/Logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Logo() {
   return (
-    <img src='https://i.ibb.co/FxQL9rv/logo-app-reservas.png' alt='App Turnos'/>
-    /* <img src='https://i.ibb.co/gZdkQnQ/ganesha-logo-app-200.png' alt='App Ganesha'/> */
+    /* <img src='https://i.ibb.co/FxQL9rv/logo-app-reservas.png' alt='App Turnos'/> */
+    <img src='https://i.ibb.co/gZdkQnQ/ganesha-logo-app-200.png' alt='App Ganesha'/>
     
   )
 }
