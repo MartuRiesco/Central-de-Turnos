d1d5a36265ecdce02637be927f7fa35e7ee760b3
martin-centurion <martinalejandrocenturion@gmail.com>
2024-05-27 03:54:49 -0300

    edit color

--- turnos/src/components/Logo.js
+++ turnos/src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Logo() {
   return (
     /* <img src='https://i.ibb.co/FxQL9rv/logo-app-reservas.png' alt='App Turnos'/> */
-    <img src='https://i.ibb.co/gZdkQnQ/ganesha-logo-app-200.png' alt='App Ganesha'/>
+    <img src="https://i.ibb.co/gZdkQnQ/ganesha-logo-app-200.png" alt="ganesha-logo-app-200" border="0" />
     
   )
 }
